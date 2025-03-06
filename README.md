b08a2f5e4bf226bbbbca794192f3b8ca03c79c9a
Li1229575912 <1229575912@qq.com>
2025-03-07 00:24:44 +0800

    hello world

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # cpp_knowledge
+
+### 开发工具
+VS Code
+
+### Linux环境：
+// 安装并检查gcc g++ gdb 版本
+sudo apt install git g++ 
+
+### Windows环境
+### MacOS和环境
+
---
+++ hello.cpp
@@ -0,0 +1,11 @@
+#include <iostream> // 包含头文件、预处理编译指令
+
+using namespace std; //指定命名空间、命名空间编译指令
+
+int main(void) //函数头，main函数是入口，是必须存在的
+{ //函数体-开始
+    cout << "hello,world";
+    // cout c是字符单词的首字母，out代表输出，这句表示字符输出语句
+    // << 叫做左移运算符，将字符串传递给cout函数
+    return 0; //返回值
+}//函数体-结束
